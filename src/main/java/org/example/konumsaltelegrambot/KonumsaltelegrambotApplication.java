d5f5985532d8a63752b7f4b01045967e44381dbc
GrkmAyarkan <grkm.ayarkan@gmail.com>
2024-08-28 11:18:36 +0300

    First Commit and Push

---
+++ src/main/java/org/example/konumsaltelegrambot/KonumsaltelegrambotApplication.java
@@ -0,0 +1,13 @@
+package org.example.konumsaltelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonumsaltelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonumsaltelegrambotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/konumsaltelegrambot/KonumsaltelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.konumsaltelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonumsaltelegrambotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
